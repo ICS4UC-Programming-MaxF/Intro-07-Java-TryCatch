b078c573ed4f944bfae710d8bbe267588d423926
Max fairman <Maximiliano.fairman@stu.ocsb.ca>
2026-09-14 13:41:27 +0000

    VolSphere.java

--- VolSphere.java
+++ VolSphere.java
@@ -1,27 +1,40 @@
+/**
+* This Program calculates the volume of a sphere based on user input for the radius.
+* It handles invalid inputs and ensures that the radius is a positive number.
+* @author  MF-ROB
+* @version 1.0
+* @since   2026-14-09
+*/
+// Java Imports
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+// Class Declaration
 public class VolSphere {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // Asks for user input of radius
         System.out.print("Enter the radius of the sphere in feet: ");
 
         try {
-            double radius = sc.nextDouble();
+            double radius = sc.nextDouble(); // Read user input for radius
 
             if (radius < 0) {
+                // If the users number is a negative number, print an error message
                 System.out.println("Error: Radius cannot be negative.");
             } else if (radius == 0) {
+                // If the users number is 0, print the volume as 0.0
                 System.out.println("The volume of the sphere is: 0.0");
             } else {
+                // the calculator for the volume of a sphere is (4/3) * π * r^3, where r is the radius of the sphere
                 double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
                 volume = Math.round(volume * 1000.0) / 1000.0;
                 System.out.println("The volume of the sphere is: " + volume);
             }
-        } catch (InputMismatchException e) {
+        } catch (InputMismatchException e) { // catches if the user didn't enter a valid value, print an error message
             System.out.println("Error: Please enter a valid numeric value for the radius.");
         } finally {
-            sc.close();
+            sc.close(); // this line closes the scanner to prevent resource leaks
         }
     }
 }
---
+++ package-info.java
@@ -0,0 +1,6 @@
+/**
+* This is the package-info for VolSphere.java.
+* @author  MF-ROB
+* @version 1.0
+* @since   Year-Month-Day
+*/
