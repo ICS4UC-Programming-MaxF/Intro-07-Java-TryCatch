75607f5c269c00e2c82995e4e1eb6b78b0bfe59c
Max fairman <Maximiliano.fairman@stu.ocsb.ca>
2026-09-21 12:40:49 +0000

    VolSphere.java

--- VolSphere.java
+++ VolSphere.java
@@ -24,12 +24,12 @@ public class VolSphere {
                 System.out.println("Error: Radius cannot be negative.");
             } else if (radius == 0) {
                 // If the users number is 0, print the volume as 0.0
-                System.out.println("The volume of the sphere is: 0.0");
+                System.out.println("The volume of the sphere is: 0.0 feet cubed.");
             } else {
                 // the calculator for the volume of a sphere is (4/3) * π * r^3, where r is the radius of the sphere
                 double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
                 volume = Math.round(volume * 1000.0) / 1000.0;
-                System.out.println("The volume of the sphere is: " + volume);
+                System.out.println("The volume of the sphere is: " + volume + " feet cubed."); // Print the volume of the sphere rounded to 3 decimal places
             }
         } catch (InputMismatchException e) { // catches if the user didn't enter a valid value, print an error message
             System.out.println("Error: Please enter a valid numeric value for the radius.");
